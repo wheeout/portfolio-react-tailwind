b427dc298422f449d68204ec8136afd2ece9ca47
Mia Abreu e Lima <xmomoon@gmail.com>
2023-10-16 13:49:01 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Josefin Sans'", "sans-serif"],
+        bebas: "bebas",
+      },
+    },
+  },
+  plugins: [],
+};
